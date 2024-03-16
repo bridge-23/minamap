2522d74d0e8587e2a22745e357dce75813224bbd
Galiusbro <ayaalsantaev@gmail.com>
2024-03-17 04:07:36 +0700

    styling v2

--- ui/src/pages/map.page.tsx
+++ ui/src/pages/map.page.tsx
@@ -5,16 +5,18 @@ function MapPage() {
   const [visitedCountries, setVisitedCountries] = useState([{ code: "ID", name: "Indonesia" }, { code: "VN", name: "Vietnam_mainland" }]);
 
   const handleCountryClick = (svgId: string) => {
-    // Extract a meaningful part of the svgId, assuming it follows a pattern like 'countries_svg__{CountryName}'
     const countryName = svgId.replace('countries_svg__', '');
 
-    // Check if the country is already visited by searching for its name in the visitedCountries array
     if (!visitedCountries.some(country => country.name === countryName)) {
-      // Find or generate a country code if necessary. This example assumes you might have a function or a mapping to get it.
-      // For simplicity, let's pretend we append to the visited list using the svgId directly.
-      const newCountry = { code: svgId, name: countryName }; // This is a placeholder. You might want to map svgId to country code/name properly.
+      const newCountry = { code: svgId, name: countryName };
       setVisitedCountries([...visitedCountries, newCountry]);
       console.log(`Country ${svgId} point added!`);
+    }
+
+    // Add class to visited country
+    const visitedCountryElement = document.getElementById(svgId);
+    if (visitedCountryElement) {
+      visitedCountryElement.classList.add('visited-country');
     }
   };
 
